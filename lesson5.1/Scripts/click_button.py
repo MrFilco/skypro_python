ff2dfef901b033ba55487b88323855f6bfcc0da0
MrFilco <afilippov@lanit.ru>
2024-07-31 12:09:05 +0300

    lesson5.1

--- lesson5.1/Scripts/click_button.py
+++ lesson5.1/Scripts/click_button.py
@@ -4,21 +4,24 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
-import time
-def handle_modal(browser):
+
+def click_add_elements(browser):
     driver = None
     if browser == 'chrome':
         driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
     elif browser == 'firefox':
         driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
     
-    driver.get("http://the-internet.herokuapp.com/entry_ad")
-    time.sleep(2)  # Wait for modal to appear
-    close_button = driver.find_element(By.XPATH, "//div[@class='modal-footer']/p")
-    close_button.click()
+    driver.get("http://the-internet.herokuapp.com/add_remove_elements/")
+    add_button = driver.find_element(By.XPATH, "//button[text()='Add Element']")
+    for _ in range(5):
+        add_button.click()
+    
+    delete_buttons = driver.find_elements(By.XPATH, "//button[text()='Delete']")
+    print(f"Number of Delete buttons: {len(delete_buttons)}")
     
     driver.quit()
 
 # Run for both browsers
-handle_modal('chrome')
-handle_modal('firefox')
+click_add_elements('chrome')
+click_add_elements('firefox')
